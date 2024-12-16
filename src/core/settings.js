1e5b2357e72ef664dabd6894bdc970b90449c6bc
ksksns <wuska220@gmail.com>
2024-12-16 09:55:08 -0800

    Add files via upload

---
+++ src/core/settings.js
@@ -0,0 +1,27 @@
+const prefix = process.env.prefix || ';'
+const status = `discord.gg/burden`;
+
+
+
+module.exports = {
+  bot: {
+    info: {
+      prefix: process.env.prefix || ';',
+      token: process.env.token,
+    },
+    options: {
+      founders: ['837726019032973372'],
+      privateMode: false,
+    },
+    presence: {
+      name: process.env.statusText || status,
+      type: 'STREAMING',
+      url: 'https://twitch.tv/pewdiepie'
+    },
+    credits: {
+      developerId: '837726019032973372',
+      developer: 'win#0006',
+      supportServer: 'https://discord.gg/kupk5SQsQe'
+    }
+  }
+}
